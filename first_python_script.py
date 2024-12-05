4443997d1f45a41ee8c3073011c5e12c71ea8328
Diogo Ferreira <74631164+DiogoBalboa@users.noreply.github.com>
2024-12-05 20:28:52 +0000

    Main menu structure
    Added the structure and functionality to each option

---
+++ first_python_script.py
@@ -0,0 +1,23 @@
+print("Please choose from the following options:")
+print("1-Read and display names")
+print("2-Add a new name")
+print("3-Exit")
+
+user_input = input()
+
+
+while user_input != "3":
+
+
+    if user_input == "1":
+        print("You chose option 1")
+        break
+    
+    elif user_input == "2":
+        print("You chose option 2")
+        break
+    
+    else:
+        print("Option not valid please choose the correct one")
+        break
+
