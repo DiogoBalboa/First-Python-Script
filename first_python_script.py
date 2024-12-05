d38064ee1d2363d5674c8801553477a34aec25f5
Diogo Ferreira <74631164+DiogoBalboa@users.noreply.github.com>
2024-12-05 21:25:04 +0000

    Added the correct function to each option

--- first_python_script.py
+++ first_python_script.py
@@ -3,21 +3,29 @@ print("1-Read and display names")
 print("2-Add a new name")
 print("3-Exit")
 
+name_input = ""
+names = ""
 user_input = input()
-
 
 while user_input != "3":
 
-
     if user_input == "1":
-        print("You chose option 1")
+        print("These are the names inside the list:")
+        with open("names.txt", "r") as file:
+            names = file.read()
+            print(names)
         break
     
     elif user_input == "2":
-        print("You chose option 2")
+        print("What name do you want to add:")
+        name_input = input()
+        with open("names.txt", "a") as file:
+            file.write(name_input + "\n")
+        print (f"{name_input} has been added to the list.")
+
         break
     
     else:
         print("Option not valid please choose the correct one")
-        break
+    
 
